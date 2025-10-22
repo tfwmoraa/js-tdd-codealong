03af9946927494e2f6fb28fba7e66c96b7a070bb
tfwmoraa <jinnettemoraa@gmail.com>
2025-10-22 18:42:06 +0300

    submitted tdd codealong

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,13 @@
+
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
     const birthYear = 1984;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    // Update expected value based on current year (2025)
+    expect(ageOfPerson).toBe(41);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
-// Your code here
+// src/utils.js
+
+// This function calculates the current age based on the year of birth.
+export function currentAgeForBirthYear(birthYear) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - birthYear;
+}
